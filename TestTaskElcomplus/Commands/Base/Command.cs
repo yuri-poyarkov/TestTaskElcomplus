f8c193b1b213e76295aa13ae1c777d88a2585fad
Юрий Поярков <poyarkov976@gmail.com>
2021-01-25 17:19:48 +0700

    Базовый класс команды

---
+++ TestTaskElcomplus/Commands/Base/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TestTaskElcomplus.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        /// <summary>
+        /// Передача управления событием(команда) классу CommandManager
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
+    }
+}

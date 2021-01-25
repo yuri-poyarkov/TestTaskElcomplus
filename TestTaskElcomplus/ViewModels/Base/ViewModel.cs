308aaf5fd44c9d8aafb9ee08b3eb73582d841a0d
Юрий Поярков <poyarkov976@gmail.com>
2021-01-25 15:41:57 +0700

    Тестовая разметка окна

--- TestTaskElcomplus/ViewModels/Base/ViewModel.cs
+++ TestTaskElcomplus/ViewModels/Base/ViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace TestTaskElcomplus.ViewModels
+namespace TestTaskElcomplus.ViewModels.Base
 {
     internal abstract class ViewModel : INotifyPropertyChanged
     {
---
+++ TestTaskElcomplus/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using TestTaskElcomplus.ViewModels.Base;
+
+namespace TestTaskElcomplus.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _title = "Тестовое задание Elcomplus";
+
+        /// <summary> Заголовок окна </summary>
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                Set(ref _title, value);
+            }
+        }
+        #endregion
+
+
+    }
+}
